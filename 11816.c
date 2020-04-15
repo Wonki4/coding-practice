c340ea6be078690506ed02714bfa6c99cba03aa1
Wongi <qordnjsrl13@gmail.com>
2020-04-15 23:54:30 +0900

    add comment

--- 11816.c
+++ 11816.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+
+
 void strlwr(char str[]);
 int main(void) {
     char str[11];
+    // the size of string is made by considering the size of null(1) and the maximum value of integer (10)
+    // After searching something about translate octal, hexdecimal, decimal into decimal interger, i can't find how to do it.
+    // i decided to use the string.
     int i = 0;
     int sum = 0;
     int value = 0;
     int temp = 0;
 
     scanf("%s", str);
+    // I made a function for lowering strings.
     strlwr(str);
     printf("%s\n", str);
+
+    // i use the property that the first string is stored at the 0th array.
     if(str[0] == '0') {
         if(str[1] == 'x') {
+            // In this case of hexdecimal
             i = 2;
             sum = 0;
             while(str[i] != 0) {
@@ -24,6 +33,7 @@ int main(void) {
                 i += 1;
             }
         }
+        //In this case of octal
         else {
             i = 1;
             sum = 0;
@@ -33,6 +43,7 @@ int main(void) {
             }
         }
     }
+    //In this case of decimal
     else {
         i = 0;
         sum = 0;
